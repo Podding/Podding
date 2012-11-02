b86dad048ca0df998044f3e175a7e44cedbe2116
Sven Pfleiderer <sg+github@roothausen.de>
2012-11-02 18:40:41 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,33 +1,39 @@
 # encoding: utf-8
 
-require 'sinatra'
-require 'slim'
 require 'bundler/setup'
+require 'sinatra/base'
+require 'slim'
+require 'less'
+require 'logger'
 
-require_relative 'minify_resources'
+require_relative 'lib/helper'
 
-class Podding < Sinatra::Application
-  enable :sessions
+class Podding < Sinatra::Base
+  enable :sessions, :static, :logging
+
+  base_dir = File.dirname(__FILE__)
+
+  set :public_folder, base_dir + '/source/assets'
+  set :views, base_dir + '/source'
 
   configure :production do
     set :clean_trace, true
     set :css_files, :blob
     set :js_files,  :blob
-
-    MinifyResources.minify_all
   end
 
   configure :development do
-    set :css_files, MinifyResources::CSS_FILES
-    set :js_files,  MinifyResources::JS_FILES
+    # ...
   end
 
   helpers do
     include Rack::Utils
     alias_method :h, :escape_html
   end
+
+  run! if app_file == $0
 end
 
-require_relative 'helpers/init'
-require_relative 'models/init'
 require_relative 'routes/init'
+require_relative 'models/init'
+require_relative 'helpers/init'
