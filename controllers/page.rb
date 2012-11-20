e5d68edbe5f7104323a0b00c1f3f090b64bd164f
Sven Pfleiderer <sg+github@roothausen.de>
2012-11-20 14:11:24 +0100

    More page controller cleanup

--- controllers/page.rb
+++ controllers/page.rb
@@ -4,7 +4,6 @@ class Podding < Sinatra::Base
 
   get "/pages/:name" do |name|
     @model = PageModel.new(name: name, path: settings.pages)
-    meta_data = @model.meta_data
     slim @model.template
   end
 
