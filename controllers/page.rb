36663e944cce31a61b226e720ff852f3e3c4d3a5
Sven Pfleiderer <sg+github@roothausen.de>
2012-11-20 13:57:54 +0100

    Clean up page rendering

--- controllers/page.rb
+++ controllers/page.rb
@@ -3,13 +3,7 @@
 class Podding < Sinatra::Base
 
   get "/pages/:name" do |name|
-    content_dir = settings.views
-    path = "#{content_dir}/pages"
-    @model = PageModel.new(name: name, path: path)
-    render_page
-  end
-
-  def render_page
+    @model = PageModel.new(name: name, path: settings.pages)
     meta_data = @model.meta_data
     slim @model.template
   end
