0fa86827f79085035f66abf866ed800477d8d237
Sven Pfleiderer <sg+github@roothausen.de>
2012-11-21 00:12:57 +0100

    Fix special page /archive

--- controllers/page.rb
+++ controllers/page.rb
@@ -2,6 +2,13 @@
 
 class Podding < Sinatra::Base
 
+  # Special pages
+
+  get "/pages/archive" do
+    @episodes = EpisodeModel.all(path: settings.episodes)
+    slim :archive
+  end
+
   get "/pages/:name" do |name|
     @page = PageModel.new(name: name, path: settings.pages)
     slim @page.template
