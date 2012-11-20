e9fdfb9f0d689dfd800e081ce6d5d117e0070192
Sven Pfleiderer <sg+github@roothausen.de>
2012-11-20 15:48:06 +0100

    Add episode model

---
+++ models/episode.rb
@@ -0,0 +1,38 @@
+# encoding: utf-8
+
+class EpisodeModel < Model
+
+  class << self
+
+    def all(options = {})
+      episode_paths = scan_episodes(options[:path])
+      episode_paths = episode_paths.last(options[:limit]) if options[:limit]
+
+      episode_paths.map do |path|
+        EpisodeModel.new(path: path)
+      end
+    end
+
+    def find(options = {})
+    end
+
+    def scan_episodes(base_path)
+      path = "#{base_path}/**/*.md"
+      Dir[path]
+    end
+
+  end
+
+  def initialize(options = {})
+    super(options)
+  end
+
+  def content_path
+    @path
+  end
+
+  def default_template
+    :episode
+  end
+
+end
