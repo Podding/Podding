0170d76249496a8ca2455b7e63e5195dad554061
Sven Pfleiderer <sg+github@roothausen.de>
2012-11-20 14:55:46 +0100

    Refactor models

--- models/page.rb
+++ models/page.rb
@@ -13,27 +13,16 @@ class PageModel < Model
 
   end
 
-  attr_reader :content, :meta_data
-
   def initialize(options = {})
-    @path = options[:path]
-    @name = options[:name]
-
-    split_content = split_content_and_meta(content_path)
-    @content = split_content[:content]
-    @meta_data = split_content[:meta_data]
+    super(options)
   end
 
   def content_path
     "#{ @path }/#{ @name }.md"
   end
 
-  def template
-    if @meta_data[:template]
-      @meta_data[:template].to_sym
-    else
-      :page
-    end
+  def default_template
+    :page
   end
 
 end
--- models/show.rb
+++ models/show.rb
@@ -2,4 +2,22 @@
 
 class ShowModel < Model
 
+  class << self
+
+    def all(options = {})
+    end
+
+    def find(options = {})
+    end
+
+  end
+
+  def initialize(options = {})
+    super(options)
+  end
+
+  def default_template
+    :show
+  end
+
 end
