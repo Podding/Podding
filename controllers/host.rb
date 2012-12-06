59641fdd01656f3c90112a4b4fd26fb2997a1224
Sven Pfleiderer <sg+github@roothausen.de>
2012-12-06 20:04:40 +0100

    Fix page controllers

--- controllers/host.rb
+++ controllers/host.rb
@@ -9,6 +9,7 @@ class Podding < Sinatra::Base
   end
 
   get "/hosts/:name" do |name|
+    @page = Page.new(name: "hosts")
     @hosts = Host.find(name: name)
     slim @hosts.first.template
   end
--- controllers/show.rb
+++ controllers/show.rb
@@ -3,11 +3,13 @@
 class Podding < Sinatra::Base
 
   get "/shows" do
+    @page = Page.new(name: "shows")
     @shows = Show.all
     slim :shows
   end
 
   get "/shows/:name" do |name|
+    @page = Page.new(name: "shows")
     @shows = Show.find(name: name)
     slim @shows.first.template
   end
