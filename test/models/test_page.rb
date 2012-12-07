e417527b32c2980fc6dd400605f0b8f1ce161596
Sven Pfleiderer <sg+github@roothausen.de>
2012-12-07 15:44:19 +0100

    Add page model test

---
+++ test/models/test_page.rb
@@ -0,0 +1,32 @@
+
+require_relative 'helper'
+
+class PageModelTest < MiniTest::Unit::TestCase
+
+  def setup
+    @pages = Page.all
+  end
+
+  def test_scan_files
+    files = Page.scan_files
+    assert_equal 4, files.count
+  end
+
+  def test_page_amount
+    assert_equal 4, @pages.count
+  end
+
+  def test_page_type
+    assert @pages.all? { |s| s.instance_of?(Page) }
+  end
+
+  def test_page_path
+    assert @pages.all? { |s| File.exist? s.path }
+  end
+
+  def test_find_page_by_name
+    assert 1, Page.find(name: "page1").count
+    assert 1, Page.find(name: "page2").count
+  end
+
+end
