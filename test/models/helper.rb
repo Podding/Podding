8a334d4b227e8014530d7ebccfc5b35e39b8e37b
Sven Pfleiderer <sg+github@roothausen.de>
2012-12-07 15:54:31 +0100

    Load all models in model tests

--- test/models/helper.rb
+++ test/models/helper.rb
@@ -4,5 +4,6 @@ require 'minitest/autorun'
 require 'minitest/benchmark'
 
 require_relative '../../lib/podding'
+require_relative '../../models/init'
 
 Model.base_path = File.dirname(__FILE__) + '/../source'
