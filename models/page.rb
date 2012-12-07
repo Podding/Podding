4f1eb64b9fe83921f983778117639c9128e1b302
Sven Pfleiderer <sg+github@roothausen.de>
2012-12-07 16:29:41 +0100

    Clean up page model

--- models/page.rb
+++ models/page.rb
@@ -3,17 +3,7 @@
 class Page < Model
 
   def initialize(options = {})
-    @name = options[:name]
-
     super(options)
-  end
-
-  def content_path
-    "#{ self.class.path }/#{ @name }.md"
-  end
-
-  def default_template
-    :page
   end
 
 end
