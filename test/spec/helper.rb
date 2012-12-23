e7c02b54f6c64d44bd108d90d677c9f71477f056
Sven Pfleiderer <sg+github@roothausen.de>
2012-12-24 00:25:22 +0100

    Move helpers test to /test/spec
    Also: Lay the groundwork for the new isolated unit test system

---
+++ test/spec/helper.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'mocha/setup'
+
+require_relative '../../lib/podding'
+require_relative '../../models/init'
+require_relative '../../helpers/init'
---
+++ test/spec/helpers/test_url_helper.rb
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+require_relative '../helper'
+
+
+describe URLs do
+  include URLs
+
+  class Content; end
+
+  it 'should generate content URLs' do
+    content = Content.new
+
+    Content.expects(:name).returns("Content")
+    content.expects(:name).returns('my_content')
+
+    url_for(content).must_equal '/contents/my_content'
+  end
+
+  it 'should generate correct episode URLs' do
+    episode = Episode.new(raw_content: '')
+    show = Show.new(raw_content: '')
+
+    episode.expects(:name).returns('my_episode')
+    show.expects(:name).returns('my_show')
+    episode.expects(:show).returns(show)
+
+    url_for(episode).must_equal '/shows/my_show/my_episode'
+  end
+
+  it 'should generate correct twitter URLs' do
+    host = mock
+    host.expects(:twitter_name).returns('my_twitter')
+    twitter_url(host).must_equal('https://twitter.com/my_twitter')
+  end
+
+  it 'should generate correct flattr URLSs' do
+    host = mock
+    host.expects(:flattr_name).returns('my_flattr')
+    flattr_url(host).must_equal('https://flattr.com/profile/my_flattr')
+  end
+
+end
+
+
