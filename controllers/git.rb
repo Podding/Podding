04ca83b8804663172864d779e2ef3af0c890aa47
Dominik Huebner <contact@dhuebner.com>
2012-12-27 02:04:35 +0100

    varnish cache invaldation gets host and port from config file

--- controllers/git.rb
+++ controllers/git.rb
@@ -8,7 +8,7 @@ class Podding < Sinatra::Base
   post "/git" do
     push = JSON.parse(params[:payload])
     
-    url = URI.parse("http://127.0.0.1:8080/")
+    url = URI.parse("http://#{settings.varnish_host}:#{settings.varnish_port} /")
     req = Net::HTTP::VarnishBan.new(url.path)
 
     res = Net::HTTP.new(url.host, url.port).start do |http|
