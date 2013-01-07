bb6ac781944d54b71725a3dd57a6d91aadedf2c6
Patrick Dinger <paxos2k@gmail.com>
2013-01-07 15:58:21 +0100

    Change order
    - git pull
    - varnish invalidation

--- controllers/git.rb
+++ controllers/git.rb
@@ -4,6 +4,8 @@ class Podding < Sinatra::Base
 
   post "/git" do
     #push = JSON.parse(params[:payload])
+
+    `git submodule foreach git pull origin master`
     
     url = URI.parse("http://#{settings.varnish_host}:#{settings.varnish_port}/")
     req = Net::HTTP::VarnishBan.new(url.path)
@@ -11,8 +13,6 @@ class Podding < Sinatra::Base
     res = Net::HTTP.new(url.host, url.port).start do |http|
       http.request(req)
     end
-
-    `git submodule foreach git pull origin master`
   end
 
 end
