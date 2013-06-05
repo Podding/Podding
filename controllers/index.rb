4bd3e0b9bfa1bd4dd929a3ed4315246c12ca4256
sirmarcel <me@lumenlog.com>
2013-06-05 14:13:30 +0200

    reintroduced the arrays, they are needed for layout purposes

--- controllers/index.rb
+++ controllers/index.rb
@@ -3,7 +3,19 @@
 class Podding < Sinatra::Base
 
   get "/" do
-    @episodes = Episode.all
+    episodes = Episode.all
+    @live_episodes = []
+    @published_episodes = []
+    @planned_episodes = []
+    for episode in episodes do |episode|
+      if episde.status == "published"
+        @published_episodes << episode
+      elsif episode.status == "live"
+        @live_episodes << episode
+      else
+        @planned_episodes << episode
+      end
+    end
     slim :index
   end
 
