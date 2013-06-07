2d03f964a2d425820caa806e052f1d4be2f2d57c
Sven Pfleiderer <sg+github@roothausen.de>
2013-06-07 23:54:35 +0200

    Update model test

--- test/spec/models/test_model.rb
+++ test/spec/models/test_model.rb
@@ -8,11 +8,13 @@ describe Model do
     lambda { Model.new({ }) }.must_raise(ArgumentError)
   end
 
-  it 'can be created with empty content' do
-    model = Model.new(raw_content: '')
-    model.must_be_instance_of Model
-    model.content.must_equal ''
-    model.data.must_equal({ })
+  it 'can\t be created with empty content' do
+    lambda { Model.new(raw_content: '') }.must_raise(RuntimeError)
+  end
+
+  it 'can\'t be created without a YAML header and content' do
+    content = "CONTENT"
+    lambda { Model.new(raw_content: content) }.must_raise(RuntimeError)
   end
 
   it 'can be created with data and empty content' do
@@ -23,64 +25,33 @@ describe Model do
   end
 
   it "can't be created with a broken YAML header" do
-    content = <<-EOF
----
-foo: [
----
-    EOF
+    content = generate_document({ 'foo' => '[' })
     lambda { Model.new(raw_content: content) }.must_raise(RuntimeError)
   end
 
   it 'can be created with a valid YAML header' do
-    content = <<-EOF
----
-foo: bar
-hurr: durr
-trolo: lala
----
-    EOF
-
-    Model.new(raw_content: content).data.must_equal({ 'foo' => 'bar', 'hurr' => 'durr', 'trolo' => 'lala' })
+    header = { 'foo' => 'bar', 'hurr' => 'durr', 'trolo' => 'lala' }
+    content = generate_document(header)
+    Model.new(raw_content: content).data.must_equal(header)
   end
 
   it 'can be created with a YAML header and content' do
-    content = <<-EOF
----
-asdf: hjkl
----
-THIS IS SPARTA!
-    EOF
-
+    content = generate_document({ 'asdf' => 'hjkl'}, 'THIS IS SPARTA!')
     Model.new(raw_content: content).content.must_equal "THIS IS SPARTA!"
   end
 
-  it 'can be created without a YAML header and content' do
-    content = "CONTENT"
-    Model.new(raw_content: content).content.must_equal "CONTENT"
-  end
-
   it 'should be invalid without a name' do
-    content = "CONTENT"
+    content = generate_document({ 'noname' => 'gnah' }, 'CONTENT')
     Model.new(raw_content: content).valid?.must_equal false
   end
 
   it 'should be valid with a name' do
-    content = <<-EOF
----
-name: dawg
----
-Content
-    EOF
+    content = generate_document({ 'name' => 'dawg' }, 'CONTENT')
     Model.new(raw_content: content).valid?.must_equal true
   end
 
   it 'should serialize a valid representation of itself' do
-    content = <<-EOF
----
-name: serialize_me
----
-Content
-    EOF
+    content = generate_document({ 'name' => 'serialize_me' }, 'CONTENT')
     model = Model.new(raw_content: content)
     new_model = Model.new(raw_content: model.serialize)
     new_model.must_equal model
