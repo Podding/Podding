58af0f81c6e42a205b388aae5f69b7b3f6c1d259
Sven Pfleiderer <sg+github@roothausen.de>
2013-06-07 23:53:58 +0200

    Add generate_document helper

--- test/spec/helper.rb
+++ test/spec/helper.rb
@@ -7,7 +7,7 @@ require 'mocha/setup'
 require 'ostruct'
 
 require 'pry'
-require 'pry-nav'
+require 'pry-debugger'
 
 require_relative '../../lib/podding'
 require_relative '../../lib/podding/storage_engines/memory_storage'
@@ -15,3 +15,12 @@ require_relative '../../models/init'
 require_relative '../../helpers/init'
 
 Model.storage_engine = MemoryStorage
+
+def generate_document(header, content = '')
+<<-EOF
+---
+#{ header.inject('') { |acc, val| acc +=  "#{ val[0] }: #{ val[1] }\n" } }
+---
+#{ content }
+EOF
+end
