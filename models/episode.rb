697d3e370d3fa5d2f442157db2b273b25656bc0e
Sven Pfleiderer <sg+github@roothausen.de>
2013-06-08 00:24:04 +0200

    Remove unused/unwanted auphonic code from episode

--- models/episode.rb
+++ models/episode.rb
@@ -49,7 +49,6 @@ class Episode < Model
       end
     end
 
-
   end
 
   def hosts
@@ -59,7 +58,7 @@ class Episode < Model
           Host.first(name: host)
         end
         # Filter nil hosts
-        hosts.select { |h| !h.nil? }
+        hosts.reject { |h| h.nil? }
       else
         Array(Host.first(name: host_names))
       end
@@ -68,33 +67,6 @@ class Episode < Model
     else
       [ ]
     end
-  end
-
-  def auphonic_metadata(username="", password="")
-    meta = {}
-    
-    if auphonic = data['auphonic']
-      auphonic.each do |audio_format, auphonic_uuid|
-        uri = URI.parse("https://auphonic.com/api/production/#{auphonic_uuid}.json")
-
-        @http=Net::HTTP.new(uri.host, 443)
-        @http.use_ssl = true
-
-        @http.start do |http|
-          req = Net::HTTP::Get.new(uri.path)
-          req.basic_auth username, password
-          response = http.request(req)
-          parsed = JSON.parse(response.body)
-          cur_metadata = {
-            length:  parsed["data"]["length_timestring"]
-          }
-
-          meta[audio_format.to_sym] = cur_metadata
-        end
-      end
-    end
-
-    meta
   end
 
   private
