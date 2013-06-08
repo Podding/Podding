11111ffe3ce90ebb4219dad3c2e80637ed6bc667
Sven Pfleiderer <sg+github@roothausen.de>
2013-06-09 01:05:18 +0200

    Refactor model tests

--- test/spec/models/test_episode.rb
+++ test/spec/models/test_episode.rb
@@ -4,94 +4,113 @@ require_relative '../helper'
 
 describe Episode do
 
-  before do
-    content = <<-EOF
----
-name: epi
-date: 24.12.2012
----
-!!!
-This is the teaser
-!!!
-This is the content
-    EOF
-
-    @episode = Episode.new(raw_content: content)
+  it 'can set the content' do
+    document = mock_document(content: 'Some content', data: { 'name' => 'epi' })
+    episode = Episode.new(document)
+    episode.content.must_equal('Some content')
   end
 
-  it 'sets the content' do
-    @episode.content.must_equal 'This is the content'
-  end
+  it 'can set the teaser' do
+    content = "!!!\nThis is the teaser\n!!!\nThis is the content"
 
-  it 'sets the teaser' do
-    @episode.teaser.must_equal 'This is the teaser'
+    document = mock_document(content: content, data: { 'name' => 'epi' })
+    episode = Episode.new(document)
+    episode.content.must_equal('This is the content')
+    episode.teaser.must_equal('This is the teaser')
   end
 
   it 'has an empty teaser when not available' do
-    content = generate_document({ foo: 'bar' })
-    Episode.new(raw_content: content).teaser.must_equal('')
+    document = mock_document(content: '', data: { 'foo' => 'bar' })
+    episode = Episode.new(document)
+    episode.content.must_equal('')
+    episode.teaser.must_equal('')
   end
 
   describe '#date' do
 
     it 'can parse the set date' do
-      @episode.date.must_be_instance_of Date
-      @episode.date.must_equal Date.new(2012, 12, 24)
+      document = mock_document(content: '', data: { 'date' => '24.12.2012' })
+      episode = Episode.new(document)
+      episode.date.must_be_instance_of Date
+      episode.date.must_equal Date.new(2012, 12, 24)
     end
 
   end
 
+  describe '#status' do
+  end
+
   describe '#hosts' do
 
-    before do
-      @asdf = Host.new(raw_content: "---\n name: asdf \n---")
-      @asdf.save
-      @hjkl = Host.new(raw_content: "---\n name: hjkl \n---")
-      @hjkl.save
-      @huh = Host.new(raw_content: "---\n name: huh \n---")
-      @huh.save
-    end
-
     it 'returns an empty array when no hosts are available' do
-      hosts = @episode.hosts
+      document = mock_document(content: '', data: { })
+      hosts = Episode.new(document).hosts
       hosts.must_be_kind_of(Enumerable)
       hosts.must_be_empty
     end
 
     it 'can reference a single host with attribute host' do
-      hosts = Episode.new(raw_content: "---\n host: asdf \n---").hosts
+      document = mock_document(content: '', data: { 'host' => 'asdf' })
+      host = Host.new(stub(content: '', data: { 'name' => 'asdf' }))
+      Host.expects(:first).with(name: 'asdf').returns(host)
+
+      hosts = Episode.new(document).hosts
       hosts.must_be_kind_of(Enumerable)
-      hosts.size.must_equal 1
+      hosts.size.must_equal(1)
+      hosts.first.must_equal(host)
     end
 
     it 'can reference a single host with attribute hosts' do
-      hosts = Episode.new(raw_content: "---\n hosts: hjkl \n---").hosts
+      document = mock_document(content: '', data: { 'hosts' => 'hjkl' })
+      host = Host.new(stub(content: '', data: { 'name' => 'hjkl' }))
+      Host.expects(:first).with(name: 'hjkl').returns(host)
+
+      hosts = Episode.new(document).hosts
       hosts.must_be_kind_of(Enumerable)
-      hosts.size.must_equal 1
-      hosts.must_include @hjkl
+      hosts.size.must_equal(1)
+      hosts.must_equal([host])
     end
 
     it 'can reference a single host with array attribute hosts' do
-      hosts = Episode.new(raw_content: "---\n hosts: [ huh ] \n---").hosts
+      document = mock_document(content: '', data: { 'hosts' => ['huh'] })
+      host = Host.new(stub(content: '', data: { 'name' => 'huh' }))
+      Host.expects(:first).with(name: 'huh').returns(host)
+
+      hosts = Episode.new(document).hosts
       hosts.must_be_kind_of(Enumerable)
       hosts.size.must_equal 1
-      hosts.must_include @huh
+      hosts.must_equal([host])
     end
 
     it 'can reference multiple hosts with array attribute hosts' do
-      hosts = Episode.new(raw_content: "---\n hosts: [ asdf, hjkl, huh ] \n---").hosts
+      document = mock_document(content: '', data: { 'hosts' => ['asdf', 'huh'] })
+      host1 = Host.new(stub(content: '', data: { 'name' => 'huh' }))
+      host2 = Host.new(stub(content: '', data: { 'name' => 'asdf' }))
+      Host.expects(:first).with(name: 'huh').returns(host1)
+      Host.expects(:first).with(name: 'asdf').returns(host2)
+
+      hosts = Episode.new(document).hosts
       hosts.must_be_kind_of(Enumerable)
-      hosts.size.must_equal 3
-      hosts.must_include @asdf
-      hosts.must_include @hjkl
-      hosts.must_include @huh
+      hosts.size.must_equal(2)
+      hosts.must_include(host1)
+      hosts.must_include(host2)
     end
 
     it 'only references valid hosts' do
-      episode = Episode.new(raw_content: "---\n hosts: [ asdf, hjkl, non_existing ] \n---")
-      hosts = episode.hosts
+      document = mock_document(
+        content: '', data: { 'hosts' => ['asdf', 'huh', 'does_not_exist'] }
+      )
+      host1 = Host.new(stub(content: '', data: { 'name' => 'huh' }))
+      host2 = Host.new(stub(content: '', data: { 'name' => 'asdf' }))
+      Host.expects(:first).with(name: 'huh').returns(host1)
+      Host.expects(:first).with(name: 'asdf').returns(host2)
+      Host.expects(:first).with(name: 'does_not_exist').returns(nil)
+
+      hosts = Episode.new(document).hosts
       hosts.must_be_kind_of(Enumerable)
-      hosts.size.must_equal 2
+      hosts.size.must_equal(2)
+      hosts.must_include(host1)
+      hosts.must_include(host2)
     end
 
   end
--- test/spec/models/test_model.rb
+++ test/spec/models/test_model.rb
@@ -2,59 +2,33 @@
 
 require_relative '../helper'
 
+class TestModel < Model
+end
+
 describe Model do
 
-  it "can't be created without content" do
-    lambda { Model.new({ }) }.must_raise(ArgumentError)
-  end
+  # todo test metaprogramming
 
-  it 'can\t be created with empty content' do
-    lambda { Model.new(raw_content: '') }.must_raise(RuntimeError)
-  end
+  describe '#initialize' do
 
-  it 'can\'t be created without a YAML header and content' do
-    content = "CONTENT"
-    lambda { Model.new(raw_content: content) }.must_raise(RuntimeError)
-  end
+    it 'should call the right document methods' do
+      data = { 'foo' => 'bar' }
+      document = mock_document(content: 'content', data: data)
+      model = Model.new(document)
+      model.content.must_equal('content')
+      model.data.must_equal(data)
+    end
 
-  it 'can be created with data and empty content' do
-    model = Model.new(raw_content: "---\n foo: bar \n---")
-    model.must_be_instance_of Model
-    model.content.must_equal ''
-    model.data.must_equal({ 'foo' => 'bar' })
-  end
+    it 'should be invalid without a name' do
+      document = stub(content: 'CONTENT', data: { 'noname' => 'a' })
+      Model.new(document).valid?.must_equal(false)
+    end
 
-  it "can't be created with a broken YAML header" do
-    content = generate_document({ 'foo' => '[' })
-    lambda { Model.new(raw_content: content) }.must_raise(RuntimeError)
-  end
+    it 'is valid with a name' do
+      document = stub(content: 'CONTENT', data: { 'name' => 'Peter' })
+      Model.new(document).valid?.must_equal(true)
+    end
 
-  it 'can be created with a valid YAML header' do
-    header = { 'foo' => 'bar', 'hurr' => 'durr', 'trolo' => 'lala' }
-    content = generate_document(header)
-    Model.new(raw_content: content).data.must_equal(header)
-  end
-
-  it 'can be created with a YAML header and content' do
-    content = generate_document({ 'asdf' => 'hjkl'}, 'THIS IS SPARTA!')
-    Model.new(raw_content: content).content.must_equal "THIS IS SPARTA!"
-  end
-
-  it 'should be invalid without a name' do
-    content = generate_document({ 'noname' => 'gnah' }, 'CONTENT')
-    Model.new(raw_content: content).valid?.must_equal false
-  end
-
-  it 'should be valid with a name' do
-    content = generate_document({ 'name' => 'dawg' }, 'CONTENT')
-    Model.new(raw_content: content).valid?.must_equal true
-  end
-
-  it 'should serialize a valid representation of itself' do
-    content = generate_document({ 'name' => 'serialize_me' }, 'CONTENT')
-    model = Model.new(raw_content: content)
-    new_model = Model.new(raw_content: model.serialize)
-    new_model.must_equal model
   end
 
 end
