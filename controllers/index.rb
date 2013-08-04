3c44afbc7d94f248b5d86827507f857148698531
sirmarcel <me@lumenlog.com>
2013-08-04 19:01:39 +0200

    Added pagination to index view controller, updated frontend accordingly

--- controllers/index.rb
+++ controllers/index.rb
@@ -2,6 +2,29 @@
 
 class Podding < Sinatra::Base
 
+  get "/page/:page" do |page|
+    @page_num = page.to_i
+    redirect to("/") if @page_num == 1
+
+    episodes = Episode.all
+    @live_episodes = [] # higher pages don't need these
+    @published_episodes = [] # higher pages don't need these
+    @planned_episodes = []
+    for episode in episodes
+      if episode.status == "published"
+        @published_episodes << episode
+      end
+    end
+
+    @paginated = true
+    @page_max = (( @published_episodes.length / settings.episodes_per_page ).to_f).ceil
+    @published_episodes = @published_episodes.slice!(@page_num,settings.episodes_per_page)
+
+    pass if @page_num > @page_max
+
+    slim :index
+  end
+
   get "/" do
     episodes = Episode.all
     @live_episodes = []
@@ -16,8 +39,16 @@ class Podding < Sinatra::Base
         @planned_episodes << episode
       end
     end
+
+
+    @paginated = true if @published_episodes.length > settings.episodes_per_page
+    @page_num = 1
+    @page_max = (( @published_episodes.length / settings.episodes_per_page ).to_f).ceil
+    @published_episodes = @published_episodes.slice!(0,settings.episodes_per_page)
     slim :index
   end
+
+
 
 end
 
