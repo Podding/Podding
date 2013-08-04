608fd0425cf71b9d763dd83f902728294b848893
sirmarcel <me@lumenlog.com>
2013-08-04 15:45:24 +0200

    Add audioformats to show model in order to use it as default

--- models/show.rb
+++ models/show.rb
@@ -5,8 +5,21 @@ class Show < Model
   attribute :title
   attribute :cover_url
   attribute :author
+  attribute :audioformats
 
   has_many :episodes, :Episode
+
+  def audioformats
+    if audioformat_names = data['audioformats']
+      if audioformat_names.is_a?(Array)
+        audioformats = Array.new
+        audioformat_names.each do |format|
+          audioformats << Audioformat.first(name: format)
+        end
+        return audioformats
+      end
+    end
+  end
 
   def live_episodes
     Episode.find(show: @data["name"], status: "live")
