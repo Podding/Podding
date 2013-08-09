f1bec6851894d21e31e939985a36a0b93fab47cd
sirmarcel <me@lumenlog.com>
2013-08-09 19:02:54 +0200

    Remove special page pages/archive because it now resides at /archive

--- controllers/page.rb
+++ controllers/page.rb
@@ -2,14 +2,6 @@
 
 class Podding < Sinatra::Base
 
-  # Special pages
-
-  get "/pages/archive" do
-    @page = Page.first(name: "archive")
-    @episodes = Episode.all
-    slim :archive
-  end
-
   get "/pages/:name" do |name|
     @page = Page.first(name: name)
     slim @page.template
