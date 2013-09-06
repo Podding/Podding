f251e4dff55968ef2527433144ecdf96d92c9bc0
sirmarcel <me@lumenlog.com>
2013-09-06 22:28:06 +0200

    Implemented support for inheriting method return values in models.

--- lib/podding/model.rb
+++ lib/podding/model.rb
@@ -100,6 +100,41 @@ class Model
     self.name.downcase
   end
 
+  # Inheriting values for methods from other models (mostly connected by belongs_to)
+
+  @@inheriting_methods = {}
+
+  def self.inherits_from(parent_method, *args)
+    args.each do |method_name|
+      @@inheriting_methods[method_name] = parent_method
+    end
+  end
+
+  def self.inheriting_for_method(name)
+    if @@inheriting_methods.keys.include? name
+      parent_method = @@inheriting_methods[name]
+      @@inheriting_methods.delete(name)
+      overwrite_inheriting_method(name, parent_method)
+    end
+  end
+
+  def self.method_added(name)
+    inheriting_for_method(name)
+  end
+
+  def self.overwrite_inheriting_method(name, parent_method)
+    original_method_name = :"#{ name }_non_inheriting"
+    alias_method original_method_name, name
+
+    define_method(name) do 
+      if eval "#{ original_method_name }.nil?"
+        eval "#{ parent_method }.#{ name }"
+      else
+        eval "#{ original_method_name }"
+      end
+    end
+  end
+
   attr_reader :document, :content, :data
 
   attribute :name
