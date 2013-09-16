bef170a552b441b2cbc0d6a193a27861cfaa31ec
sirmarcel <me@lumenlog.com>
2013-09-16 13:30:27 +0200

    Build basic markdown filter

---
+++ filters/markdown.rb
@@ -0,0 +1,12 @@
+# A TextFilter that converts markdown to html using redcarpet
+
+require 'redcarpet'
+
+class MarkdownFilter < TextFilter
+	priority 0
+
+	def render(content)
+		renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
+		renderer.render(content)
+	end
+end
