0f61e2de20e960c6c65309ec5faf16192b99b5da
sirmarcel <me@lumenlog.com>
2013-09-18 15:48:44 +0200

    Implement filter to generate amazon urls

---
+++ filters/amazon_urls.rb
@@ -0,0 +1,34 @@
+# A TextFilter that generates Amazon urls
+# Usage: [Product Name]( ASIN(10 characters, uppercase letters or numbers) )
+# In config.yaml specify: amazon_id (your affiliate id), amazon_base_url (for example: https://amazon.de)
+
+class AmazonUrls < TextFilter
+  expects :markdown
+
+  def render(content)
+    asin = /\(\s*([(\d)(A-Z)]{10})\s*\)/
+
+    # Set defaults
+    amazon_base_url = "https://amazon.com"
+    amazon_id = nil
+
+    amazon_base_url = Settings["amazon_base_url"] if Settings["amazon_base_url"]
+    amazon_id = Settings["amazon_id"] if Settings["amazon_id"]
+
+    content.gsub(asin) do |match|
+      amazon_link = "( "
+      amazon_link += amazon_base_url
+      amazon_link += "/dp/"
+      amazon_link += match[1..10]
+      if amazon_id
+        amazon_link += "?tag="
+        amazon_link += amazon_id
+      end
+      amazon_link += " )"
+
+      amazon_link
+    end
+  end
+end
+
+TextFilterEngine.register_filter(AmazonUrls)
