489c5181d464144c83466886f3d17d612b16b500
sirmarcel <me@lumenlog.com>
2013-09-23 10:27:21 +0200

    Implement filter for "lazy urls"

---
+++ filters/lazy_urls.rb
@@ -0,0 +1,23 @@
+# A TextFilter for lazy urls in Markdown
+#
+# Usage:
+
+# - Link Text http://url.com/fooo
+#   + Another Link in the List http://foo.org
+#   + Yet another link https://blah.com
+#     * One level deeper! http://foo.com/blah
+
+class LazyUrls < TextFilter
+  expects :text
+
+  def render(content)
+    lazyurl = /^ (\s*) ([-+\*]) (\s*) (.*) (\s) (https?:\/(\/\S+)+) $/x
+
+    return content.gsub(lazyurl) do |match|
+      "#{ $1 }#{ $2 } [#{ $4 }]( #{ $6 } )"
+    end
+  end
+
+end
+
+TextFilterEngine.register_filter(LazyUrls)
