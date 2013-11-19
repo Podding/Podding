d0292a02abe8239d360ca55f38c659cea327ffcb
sirmarcel <me@lumenlog.com>
2013-11-19 21:01:48 +0100

    Atom -> RSS

--- controllers/feed.rb
+++ controllers/feed.rb
@@ -2,10 +2,13 @@
 
 class Podding < Sinatra::Base
   get "/feed/:show_name/:audio_format/feed.xml" do |show_name, audio_format|
-    @episodes = Episode.find_match(show: show_name)
+    @episodes = []
+    Episode.all.each do |episode|
+      @episodes << episode if episode.audioformats.include?(audio_format) and episode.show == show_name
+    end
     @show = Show.first(name: show_name)
     @audio_format = audio_format
-    builder :atom
+    builder :rss
   end
 
   get "/feed/:audio_format/feed.xml" do |audio_format|
@@ -15,6 +18,6 @@ class Podding < Sinatra::Base
     end
     @audioformat = Audioformat.first(name: audio_format)
     @episodes.inspect.to_s
-    builder :atom
+    builder :rss
   end
 end
