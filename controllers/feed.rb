4ea86bc2afe8bb7999430e6b22c2e0a3f20c5035
sirmarcel <me@lumenlog.com>
2013-11-19 20:47:34 +0100

    Use crappy method to find episodes, but it works.

--- controllers/feed.rb
+++ controllers/feed.rb
@@ -9,8 +9,11 @@ class Podding < Sinatra::Base
   end
 
   get "/feed/:audio_format/feed.xml" do |audio_format|
-    @episodes = Episode.find_match(:audioformats => audio_format)
-    @audio_format = audio_format
+    @episodes = []
+    Episode.all.each do |episode|
+      @episodes << episode if episode.audioformats.include?(audio_format)
+    end
+    @audioformat = Audioformat.first(name: audio_format)
     @episodes.inspect.to_s
     builder :atom
   end
