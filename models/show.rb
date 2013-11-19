ef294ffd7ef534434f0ab4a6dc1405d0e773de22
sirmarcel <me@lumenlog.com>
2013-11-19 20:51:32 +0100

    Update author

--- models/show.rb
+++ models/show.rb
@@ -4,10 +4,14 @@ class Show < Mlk::Model
 
   attribute :title
   attribute :cover_url
-  attribute :author
   attribute :audioformats
 
   has_many :Episode, :episodes
+
+  # TODO: Use better default mechanism
+  def author
+    @data["author"] ? @data["author"] : Settings.author
+  end
 
   def live_episodes
     Episode.find(show: @data["name"], status: "live")
